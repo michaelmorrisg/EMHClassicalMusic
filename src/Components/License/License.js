5d53ce45b98c6f57fa37b9d5b83e8c96f694c08a
Mike Morris <michaelmorrisg@gmail.com>
2018-10-09 08:58:38 -0600

    styling on licensing page

--- src/Components/License/License.js
+++ src/Components/License/License.js
@@ -7,12 +7,16 @@ class License extends Component {
         return(
             <div className="licensing-main">
                 <div className="licensing-content">
-                    <h1 className="page-header">Licensing</h1>
-                    <p>For licensing, please contact Warner/Chappell Production Music</p>
-                    <p>(615) 244-6515</p>
-                    <p>licensing@warnerchappellpm.com</p>
-                    <img src={require('../../Assets/warnerLogo.png')}/>
+                    <div className="licensing-left">
+                        <h1 className="page-header">Licensing</h1>
+                    </div>
+                    <div className="licensing-right">
+                        <p>For licensing, please contact Warner/Chappell Production Music</p>
+                        <p>(615) 244-6515</p>
+                        <p>licensing@warnerchappellpm.com</p>
+                    </div>
                 </div>
+                    <img src={require('../../Assets/warnerLogo.png')}/>
             </div>
         )
     }

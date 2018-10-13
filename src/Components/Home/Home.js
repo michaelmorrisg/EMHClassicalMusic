cf55b7d0757d2c1acee2abef10626a80926714cf
Mike Morris <michaelmorrisg@gmail.com>
2018-10-12 20:28:06 -0600

    fixed stream links

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -38,13 +38,13 @@ class Home extends Component {
                         <h2>Stream</h2>
                         <div className="banner-stream-icons-lower">
                                 <div className="top-row">
-                                    <a onClick={()=>this.leadTrack()} href=""><img className="grow" src={require('../../Assets/apple_music.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href=""><img className="grow" src={require('../../Assets/google_play.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href=""><img className="grow" src={require('../../Assets/spotify.svg')}/></a>
+                                    <a onClick={()=>this.leadTrack()} href="https://itunes.apple.com/us/album/christmas-a-la-vivaldi/1434635644"><img className="grow" src={require('../../Assets/apple_music.svg')}/></a>
+                                    <a onClick={()=>this.leadTrack()} href="https://play.google.com/store/music/album/EMH_Classical_Music_Czech_Symphony_Chamber_Group_C?id=Bzq2myrbhfvdheb6i7b6a53yqfu&hl=en"><img className="grow" src={require('../../Assets/google_play.svg')}/></a>
+                                    <a onClick={()=>this.leadTrack()} href="https://open.spotify.com/album/5Pkgb0P3Sh9TPl14IBsyE2"><img className="grow" src={require('../../Assets/spotify.svg')}/></a>
                                 </div>
                                 <div className="bottom-row">
-                                    <a onClick={()=>this.leadTrack()} href=""><img className="grow" src={require('../../Assets/deezer.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href=""><img className="grow" src={require('../../Assets/amazon_music.svg')}/></a>
+                                    <a onClick={()=>this.leadTrack()} href="https://www.deezer.com/album/72161212?utm_source=deezer&utm_content=album-72161212&utm_term=2363008628_1539218101&utm_medium=web"><img className="grow" src={require('../../Assets/deezer.svg')}/></a>
+                                    <a onClick={()=>this.leadTrack()} href="https://www.amazon.com/Trip-Prague-City-Philharmonic/dp/B00G92SJVU/ref=sr_1_1?s=dmusic&ie=UTF8&qid=1539099823&sr=1-1-mp3-albums-bar-strip-0&keywords=a+trip+to+Prague"><img className="grow" src={require('../../Assets/amazon_music.svg')}/></a>
                                 </div>
                             </div>
                     </div>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -31,9 +31,9 @@ class Nav extends Component{
                         <div className={this.state.expanded ? "streaming showing" : ' streaming hidden'} onMouseEnter={()=>this.setState({expanded: true})} onMouseLeave={()=>this.setState({expanded: false})}>
                             <ul>
                                 <li><a onClick={()=>this.leadTrack()} href="https://open.spotify.com/album/5Pkgb0P3Sh9TPl14IBsyE2">Spotify</a></li>
-                                <li><a onClick={()=>this.leadTrack()} href="https://itunes.apple.com/us/album/christmas-a-la-vivaldi/1434635644">iTunes</a></li>
+                                <li><a onClick={()=>this.leadTrack()} href="https://itunes.apple.com/us/album/christmas-a-la-vivaldi/1434635644">Apple Music</a></li>
                                 <li><a onClick={()=>this.leadTrack()} href="https://play.google.com/store/music/album/EMH_Classical_Music_Czech_Symphony_Chamber_Group_C?id=Bzq2myrbhfvdheb6i7b6a53yqfu&hl=en">Google Play</a></li>
-                                <li><a onClick={()=>this.leadTrack()} href="https://spotify.com">Deezer</a></li>
+                                <li><a onClick={()=>this.leadTrack()} href="https://www.deezer.com/album/72161212?utm_source=deezer&utm_content=album-72161212&utm_term=2363008628_1539218101&utm_medium=web">Deezer</a></li>
                                 <li><a onClick={()=>this.leadTrack()} href="https://www.amazon.com/Trip-Prague-City-Philharmonic/dp/B00G92SJVU/ref=sr_1_1?s=dmusic&ie=UTF8&qid=1539099823&sr=1-1-mp3-albums-bar-strip-0&keywords=a+trip+to+Prague">Amazon Music</a></li>
                             </ul>
                         </div>

6f1f7398546a1ce302c9bd1e3a59d17962512467
Mike Morris <michaelmorrisg@gmail.com>
2018-10-29 12:19:43 -0600

    fixed banner to avoid carousel refresh

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import "./Banner.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import BannerLeft from '../BannerLeft/BannerLeft'
 import { Carousel } from 'react-responsive-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faGooglePlay, faItunes, faSpotify, faBandcamp} from '@fortawesome/free-brands-svg-icons'
@@ -11,17 +12,7 @@ class Banner extends Component {
         this.state = {
             picId: 'one',
             arr: ['one', 'two', 'three', 'four', 'five'],
-            white: false
         }
-    }
-
-    leadTrack(){
-        window.fbq('track', 'Lead')
-        window.pintrk('track', 'lead')
-    }
-    purchaseTrack(){
-        window.fbq('track', 'Purchase');
-        window.pintrk('track', 'checkout')
     }
 
 
@@ -31,35 +22,7 @@ class Banner extends Component {
             <div className="banner-main">
                 {/* <img src="https://f4.bcbits.com/img/a2606312365_10.jpg"/> */}
                 <div className="banner-content">
-                <div className="banner-left">
-                <div className="banner-stream">
-                        <div className="stream-content" onMouseEnter={()=>this.setState({white: true})} onMouseLeave={()=>this.setState({white:false})}>
-                            <div>
-                                <h1>Stream Now</h1>
-                            </div>
-                            <div className="banner-stream-icons">
-                                {/* <FontAwesomeIcon icon={faGooglePlay} size="3x"/>
-                                <FontAwesomeIcon icon={faSpotify} size="3x"/>
-                                <FontAwesomeIcon icon={faItunes} size="3x"/> */}
-                                <div className="top-row">
-                                    <a onClick={()=>this.leadTrack()} href="https://itunes.apple.com/us/album/christmas-a-la-vivaldi/1434635644"><img className="grow" src={this.state.white? require('../../Assets/apple_white.svg') : require('../../Assets/apple_music.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href="https://play.google.com/store/music/album/EMH_Classical_Music_Czech_Symphony_Chamber_Group_C?id=Bzq2myrbhfvdheb6i7b6a53yqfu&hl=en"><img className="grow" src={require('../../Assets/google_play.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href="https://open.spotify.com/album/5Pkgb0P3Sh9TPl14IBsyE2"><img className="grow" src={require('../../Assets/spotify.svg')}/></a>
-                                </div>
-                                <div className="bottom-row">
-                                    <a onClick={()=>this.leadTrack()} href="https://www.deezer.com/album/72161212?utm_source=deezer&utm_content=album-72161212&utm_term=2363008628_1539218101&utm_medium=web"><img className="grow" src={this.state.white? require('../../Assets/deezer_white.svg') : require('../../Assets/deezer.svg')}/></a>
-                                    <a onClick={()=>this.leadTrack()} href="https://www.amazon.com/Trip-Prague-City-Philharmonic/dp/B00G92SJVU/ref=sr_1_1?s=dmusic&ie=UTF8&qid=1539099823&sr=1-1-mp3-albums-bar-strip-0&keywords=a+trip+to+Prague"><img className="grow" src={require('../../Assets/amazon_music.svg')}/></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="banner-buy">
-                        <a onClick={()=>this.purchaseTrack()} href="https://emhclassicalmusic.bandcamp.com/"><div className="buy-content">
-                            <h1>Buy Albums</h1>
-                        </div></a>
-                                {/* <FontAwesomeIcon icon={faBandcamp} size="3x"/> */}
-                    </div>
-                </div>
+                    <BannerLeft />
                 <div className="banner-right">
                     <Carousel autoPlay={true} infiniteLoop={true} showIndicators={false} showArrows={false} showStatus={false} showThumbs={false} stopOnHover={false} transitionTime={0} interval={4000}>
                     <div id='one'>
---
+++ src/Components/BannerLeft/BannerLeft.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+
+class BannerLeft extends Component {
+    constructor(){
+        super()
+        this.state = {
+            white: false
+        }
+    }
+
+    leadTrack(){
+        window.fbq('track', 'Lead')
+        window.pintrk('track', 'lead')
+    }
+    purchaseTrack(){
+        window.fbq('track', 'Purchase');
+        window.pintrk('track', 'checkout')
+    }
+
+    render(){
+        return(
+            <div className="banner-left">
+            <div className="banner-stream">
+                    <div className="stream-content" onMouseEnter={()=>this.setState({white: true})} onMouseLeave={()=>this.setState({white:false})}>
+                        <div>
+                            <h1>Stream Now</h1>
+                        </div>
+                        <div className="banner-stream-icons">
+                            {/* <FontAwesomeIcon icon={faGooglePlay} size="3x"/>
+                            <FontAwesomeIcon icon={faSpotify} size="3x"/>
+                            <FontAwesomeIcon icon={faItunes} size="3x"/> */}
+                            <div className="top-row">
+                                <a onClick={()=>this.leadTrack()} href="https://itunes.apple.com/us/album/christmas-a-la-vivaldi/1434635644"><img className="grow" src={this.state.white? require('../../Assets/apple_white.svg') : require('../../Assets/apple_music.svg')}/></a>
+                                <a onClick={()=>this.leadTrack()} href="https://play.google.com/store/music/album/EMH_Classical_Music_Czech_Symphony_Chamber_Group_C?id=Bzq2myrbhfvdheb6i7b6a53yqfu&hl=en"><img className="grow" src={require('../../Assets/google_play.svg')}/></a>
+                                <a onClick={()=>this.leadTrack()} href="https://open.spotify.com/album/5Pkgb0P3Sh9TPl14IBsyE2"><img className="grow" src={require('../../Assets/spotify.svg')}/></a>
+                            </div>
+                            <div className="bottom-row">
+                                <a onClick={()=>this.leadTrack()} href="https://www.deezer.com/album/72161212?utm_source=deezer&utm_content=album-72161212&utm_term=2363008628_1539218101&utm_medium=web"><img className="grow" src={this.state.white? require('../../Assets/deezer_white.svg') : require('../../Assets/deezer.svg')}/></a>
+                                <a onClick={()=>this.leadTrack()} href="https://www.amazon.com/Trip-Prague-City-Philharmonic/dp/B00G92SJVU/ref=sr_1_1?s=dmusic&ie=UTF8&qid=1539099823&sr=1-1-mp3-albums-bar-strip-0&keywords=a+trip+to+Prague"><img className="grow" src={require('../../Assets/amazon_music.svg')}/></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="banner-buy">
+                    <a onClick={()=>this.purchaseTrack()} href="https://emhclassicalmusic.bandcamp.com/"><div className="buy-content">
+                        <h1>Buy Albums</h1>
+                    </div></a>
+                            {/* <FontAwesomeIcon icon={faBandcamp} size="3x"/> */}
+                </div>
+            </div>
+        )
+    }
+}
+export default BannerLeft

27e48b234c5bcb98bf8b398bd727a7d151841eb7
Mike Morris <michael.morris@getweave.com>
2022-11-30 22:45:06 -0700

    revert react router dom back

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import License from './Components/License/License';
 import Tour from './Components/Tour/Tour';
 
 export default (
-    <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/licensing" element={<License />} />
-        <Route path="/tour-2022" element={<Tour />} />
-    </Routes>
+    <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="/about">
+            <About />
+        </Route>
+        <Route path="/licensing">
+            <License />
+        </Route>
+        <Route path="/tour-2022">
+            <Tour />
+        </Route>
+    </Switch>
 );
